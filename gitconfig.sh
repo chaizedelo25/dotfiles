2b7671383f21a099f168cf8a733c7a131e3cddab
chaizedelo25 <chaizedeloach23@gmail.com>
2024-03-21 21:19:53 +0000

    updated gitconfig

--- gitconfig.sh
+++ gitconfig.sh
@@ -1,11 +1,17 @@
-[user]
+# Grabbing the user name and email 
+[user] 
          email = chaizedeloach23@gmail.com
 	 name = chaizedelo25
-
+# Making editor into vim
 [core]
          editor = vim
 
+#Autocorrect git
 [autocorrect]
-          autocorrect time = 40
+          autocorrect  = 40
+
+#Linking 
+ln -s /home/$ME/dotfiles/gitfiles/.gitconfig /home/$ME/.gitconfig
+ln -s /home/$ME/dotfiles/gitfiles/.gitmessage.txt /home/$ME/.gitmessage.txt
 
             
